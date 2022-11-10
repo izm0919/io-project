62ace5318c99c45c67283122810fe4c045b3f198
shunsuke izumi <shunsuke.izm0919@gmail.com>
2022-11-10 19:31:20 +0900

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "dynamodeeab4f3": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "ioproject7b3ee50f": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "api4bf54dfd": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
