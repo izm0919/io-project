dd9fe8926dbb529b39e1f6b9d0eb190bcf40c38e
shunsuke izumi <shunsuke.izm0919@gmail.com>
2022-11-10 19:58:19 +0900

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/amplify.js', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -68,8 +69,4 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
-
-  plugins: [
-    { src: '~/plugins/amplify.js', ssr: false }
-  ],
 }
